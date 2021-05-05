bcf07484ae89737e3aa1fc2a80e9f6cde6c474ca
athulgopal <athulgopal@athuls-mbp.lan>
2021-05-05 16:26:42 -0700

    Formatting

--- OrangeJava/src/com/java/groom/array/FindInRotatedArray.java
+++ OrangeJava/src/com/java/groom/array/FindInRotatedArray.java
@@ -9,41 +9,51 @@ write code to find an element in the array. The array was originally sorted in i
 
 public class FindInRotatedArray {
 
-	public  int smallest(int[]arr, int left, int right) {
+	public int smallest(int[] arr, int left, int right) {
 
-		if(left > right) return arr[0];
-		if(left == right) return arr[left];
-		
-		int mid=(left+right)/2;
-		
-		if(mid > left && arr[mid] < arr[mid-1]) return mid;
-		if(mid < right && arr[mid] > arr[mid+1]) return mid+1;
-		
-		if(arr[right] > arr[mid]) return smallest(arr, left, mid-1);
-		else return smallest(arr, mid+1, right);
-	}
-	
-	private int searchBinary (int[] arr, int left, int right, int searchVal) {
-		
+		if (left > right)
+			return arr[0];
+		if (left == right)
+			return arr[left];
+
 		int mid = (left + right) / 2;
-		if (left > right || left < 0 || right > arr.length) return -1;
-		if (arr[mid] == searchVal) return mid;
-		if (searchVal < arr[mid]) return searchBinary(arr, left, mid - 1, searchVal);
-		else return searchBinary(arr, mid + 1, right, searchVal);
+
+		if (mid > left && arr[mid] < arr[mid - 1])
+			return mid;
+		if (mid < right && arr[mid] > arr[mid + 1])
+			return mid + 1;
+
+		if (arr[right] > arr[mid])
+			return smallest(arr, left, mid - 1);
+		else
+			return smallest(arr, mid + 1, right);
 	}
-	
-	public int search (int[] input, int searchVal) {
+
+	private int searchBinary(int[] arr, int left, int right, int searchVal) {
+
+		int mid = (left + right) / 2;
+		if (left > right || left < 0 || right > arr.length)
+			return -1;
+		if (arr[mid] == searchVal)
+			return mid;
+		if (searchVal < arr[mid])
+			return searchBinary(arr, left, mid - 1, searchVal);
+		else
+			return searchBinary(arr, mid + 1, right, searchVal);
+	}
+
+	public int search(int[] input, int searchVal) {
 		int smallIndex = smallest(input, 0, input.length);
-		if (searchVal > input[input.length -1 ])
-			return(searchBinary(input, 0, smallIndex, searchVal));
-		else 
-			return(searchBinary(input, smallIndex, input.length, searchVal));
+		if (searchVal > input[input.length - 1])
+			return (searchBinary(input, 0, smallIndex, searchVal));
+		else
+			return (searchBinary(input, smallIndex, input.length, searchVal));
 	}
-	
+
 	public static void main(String[] args) {
-		FindInRotatedArray array=new FindInRotatedArray();
-		int[] input = new int[] {5,6,7,8,9,10,1,2,3,4};
+		FindInRotatedArray array = new FindInRotatedArray();
+		int[] input = new int[] { 5, 6, 7, 8, 9, 10, 1, 2, 3, 4 };
 		System.err.println(array.search(input, 5));
 	}
-	
+
 }
--- OrangeJava/src/com/java/test/RotateArrayTest.java
+++ OrangeJava/src/com/java/test/RotateArrayTest.java
@@ -7,22 +7,22 @@ import org.junit.Test;
 import com.java.groom.array.FindInRotatedArray;
 
 public class RotateArrayTest {
-	
+
 	FindInRotatedArray arrayTest;
-	
+
 	@Before
 	public void setUp() throws Exception {
-		 arrayTest = new FindInRotatedArray();
-	 }
+		arrayTest = new FindInRotatedArray();
+	}
 
 	@Test
 	public void test() {
-		int[] input = new int[] {5,6,7,8,9,10,1,2,3,4};
-		assertEquals(arrayTest.search(input,4), 9);
-		assertEquals(arrayTest.search(input,100), -1);
-		assertEquals(arrayTest.search(input,10), 5);
-		assertEquals(arrayTest.search(input,1), 6);
-		assertEquals(arrayTest.search(input,5), 0);
+		int[] input = new int[] { 5, 6, 7, 8, 9, 10, 1, 2, 3, 4 };
+		assertEquals(arrayTest.search(input, 4), 9);
+		assertEquals(arrayTest.search(input, 100), -1);
+		assertEquals(arrayTest.search(input, 10), 5);
+		assertEquals(arrayTest.search(input, 1), 6);
+		assertEquals(arrayTest.search(input, 5), 0);
 	}
 
 }
